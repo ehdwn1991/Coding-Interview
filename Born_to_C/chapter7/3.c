1ee30e78f762610b2b403cefa9f8c24e1956f674
Edward <ehdwn1991@gmail.com>
2018-03-09 15:44:02 +0900

    chpater7

---
+++ Born_to_C/chapter7/3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	int count=0;
+	for(int i=0;i<4;i++){
+		for(int k=0;k<count;k++) printf(" ");
+		for(int j=(2*count);j<7;j++){
+			printf("*");
+		
+		}
+	count++;
+	printf("\n");
+
+}
+
+}
