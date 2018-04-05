a6dd16839a64464254fbf816e306c05ce95c97d0
EdwardSon <ehdwn1991@gmail.com>
2018-04-05 16:06:50 +0900

    Mac

--- Born_to_C/Part1/chapter9/8.c
+++ Born_to_C/Part1/chapter9/8.c
@@ -5,8 +5,7 @@ int main(){
 	int input_num,result;
 	printf("Input your number : ");
 	scanf("%d",&input_num);
-	printf("sum of 1 to %d = %d\n",input_num,n_addition(input_num));
-	
+	printf("sum of 1 to %d = %d\n",input_num,n_addition(input_num)); 	
 }
 int n_addition(int n){
 	if(n<1)
