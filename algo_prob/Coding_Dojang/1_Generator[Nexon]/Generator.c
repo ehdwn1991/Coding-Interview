e06c8b9c8b337ff291bba58585e9489e8b050929
Son Dong ju <ehdwn1991@gmail.com>
2018-04-09 09:43:19 +0900

    W

--- algo_prob/Coding_Dojang/1_Generator[Nexon]/Generator.c
+++ algo_prob/Coding_Dojang/1_Generator[Nexon]/Generator.c
@@ -1,63 +1,50 @@
 #include <stdio.h>
-int generate();
-int main(){
+int generate(int n_times);
+int main() {
 
-    int start_range,end_range;
-    int n_digits=10;
-    scanf("%d %d",&start_range,&end_range);
-    int sum_of_generator;
-    int sum_of_all,sum_of_self;
-    int check_generator[5000]={0,};
-    int generate_state;
-    for(int i=start_range;i<=end_range;i++){
-        if(i%10==0){
-            n_digits*=10;
-        }
+	int n_digits = 10;
+	int sum_of_generator = 0;
+	int sum_of_all = 0, sum_of_self = 0;
+	int check_generator[10000] = { 0, };
+	int generate_state=0;
+	for (int i = 1; i <= 10000; i++) {
+		generate_state = generate(i);
+		
 
-            generate_state=generate(i,n_digits);
-        if(check_generator[generate_state]==0){
-            check_generator[generate_state-1]=1;
-        
-        }
+		if (check_generator[generate_state-1] == 0) {
+			check_generator[generate_state-1] = 1;
+		}
+		else { continue; }
 
-    }
-    printf("qwe");
-    for(int q=0;q<5000;q++){
-        if(check_generator[q]==0){
-            printf("%d ",(q+1));
-            sum_of_self+=(q+1);
-        }
-        
-    
-        }
-    printf("sum_of_generator :%d \n",sum_of_self);
+	}
+
+	for (int q = 1; q<=10000; q++) {
+		if (check_generator[q-1] == 0) {
+			printf("%d\n", (q));
+		}
+	}
+	
+	
 
 
 
 }
 
 
-int generate(int n_times,int n_digits){
-    int now_decimal=n_times,sum_of_generator;
-    int digits=n_times,run=0;
-    while(digits>=1){
-       digits/=10;
-        
-       run++;
-    }
-    digits=1;
-    while(run>0){
-        
-        sum_of_generator+=(now_decimal/digits);
-        
-        printf("n:%d\n",now_decimal/digits);
-        now_decimal=now_decimal-((now_decimal/digits)*digits);
-         printf("%d\n",now_decimal);   
-        //printf("sum :%d n_digits :%d \n",now_decimal,digits);
-        digits*=10;
-        run--;
-    }
-    
-    return sum_of_generator+=n_times;
+int generate(int n_times) {
+	int now_decimal = n_times, sum_of_generator=0;
+	int digits = n_times, run = 1;
+	while (digits >= 1) {
+		digits /= 10;
+
+		run*=10;
+	}
+	while (run>=1) {
+		sum_of_generator += now_decimal / run;
+		now_decimal -= (now_decimal / run)*run;
+		run /= 10;
+	}
+	
+	return sum_of_generator += n_times;
 
 }
