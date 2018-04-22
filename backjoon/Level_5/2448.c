b02b76847dedf5835175af280ea4cbf5d3f397e9
EdwardSon <ehdwn1991@gmail.com>
2018-04-22 23:50:07 +0900

    fix

--- backjoon/Level_5/2448.c
+++ backjoon/Level_5/2448.c
@@ -1,41 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// int main(){
-// 	// char asteric_module[3][5]={{' ',' ','*',' ',' '},
-// 	// 							{' ','*',' ','*',' '},
-// 	// 							{'*','*','*','*','*'}};
 
-// 	char asteric_module[][5]={"  *  "," * * ","*****"};
-// 	int N=0,center;
-// 	char a[1000][2000]={' ',};	
-// 	// for (int i = 0; i < 3; ++i)
-// 	// {
-// 	// 	printf("%s\n",asteric_module[i]);
-// 	// }
+int main(){
+    int N;
+    // printf("!!!!!");
+    // int a[3072][6143]={0,};
+    scanf("%d",&N);
+    int **row=(int**)malloc(sizeof(int*)*N),a[8][8];
+    for (int i = 0; i < N; ++i)
+    {
+       row[i]=(int*)malloc(sizeof(int)*((2*N)-1));
+       memset(row[i],0,sizeof(int)*((2*N)-1));
+   }
+    // a[0][N-1]=a[1][N-2]=a[1][N]=a[2][N-3]=a[2][N-2]=a[2][N-1]=a[2][N]=a[2][N+1]=1;
+    //1은 별 그리고 0은 공백
 
-// 	scanf("%d",&N);
-// 	a[0][N]='*';
-// 	for (int i = 1; i < N; ++i)
-// 	{
-// 		a[i][0]='*';
-// 		a[i][i]='*';
-// 	}
+   row[0][N-1]=row[1][N-2]=row[1][N]=row[2][N-3]=row[2][N-2]=row[2][N-1]=row[2][N]=row[2][N+1]=1;
 
-// 	printf("###%c###\n",a[0][3]);
-// 	printf("%s\n",a[0] );
-// 	//puts(a[0]);
-	
-// }
-int n, a[3072][6146];
-int main() {
-    scanf("%d", &n);
-    a[0][n - 1] = a[1][n - 2] = a[1][n] = a[2][n - 3] = a[2][n - 2] = a[2][n - 1] = a[2][n] = a[2][n + 1] = 1;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < 2 * n - 1; j++) {
-            if (i > 2) a[i][j] = (j>2 && a[i - 3][j - 3]) ^ a[i - 3][j + 3];
-            putchar(" *"[a[i][j]]);
+    // row[0][N-1]=1;
+    // row[1][N-2]=1;
+    // printf("%d   %d \n",row[0][N-1],row[1][N-2] );
+   for (int i = 0; i <N ; ++i)
+   {
+
+    for (int j = 0; j <=(N-1+i) ; ++j)
+    {
+        if(i>2){
+            row[i][j]=((j>2&&row[i-3][j-3])^row[i-3][j+3]);
         }
-        puts("");
     }
-    return 0;
+
+}
+
+for (int q = 0; q < N; ++q)
+{
+    for (int w = 0; w < 2*N-1; ++w)
+    {
+            // printf("%d ",row[q][w] );
+        if(row[q][w]==1)
+            printf("*");
+        else
+            printf(" ");
+    }
+    puts("");
+}
+
+free(row);
+
 }
