a671615280f47ab0b39674e39649d67735f6b085
ehdwn1991 <ehdwn1991@gmail.com>
2020-02-03 18:42:44 +0900

    TestCase

---
+++ backjoon/Samsung/17142.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int vMap[51][51]={0,};
+int main(){
+    int N,M;
+    int row=0,col=0;
+    scanf("%d %d",&N,&M);
+    while((scanf("%d",vMap[row][col]))!=EOF){
+
+
+    }
+
+}
