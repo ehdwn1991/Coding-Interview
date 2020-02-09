5cc9d77f517c299f7ece0e6c6f2bc16a8cd9ca22
ehdwn1991 <ehdwn1991@gmail.com>
2020-02-09 11:03:11 +0900

    17142

--- backjoon/Samsung/17142.c
+++ backjoon/Samsung/17142.c
@@ -1,14 +1,68 @@
 #include <stdio.h>
 #include <string.h>
 
-int vMap[51][51]={0,};
-int main(){
-    int N,M;
-    int row=0,col=0;
-    scanf("%d %d",&N,&M);
-    while((scanf("%d",vMap[row][col]))!=EOF){
 
+/*  
+DFS
+
+Queue has vertex location and next searching node info
+
+*/
+
+typedef struct queue{
+    int x;
+    int y;
+}QUEUE;
+
+int vMap[51][51]={0,};
+QUEUE qu[51];
+int rear=0;
+int qIsEmpty(){
+    if(){
 
     }
+}
+
+int qIsFull(){
+
+}
+
+int pushQ(){
+
+}
+int popQ(){
+
+}
+
+int main(){
+    int N,M,i=0,j=0;
+    int row=0,col=0;
+    scanf("%d %d",&N,&M);
+    while((scanf("%d",&vMap[row][col++]))!=EOF){
+        if(col==N){
+            col=0;
+            row++;
+        }
+        qu[row].x=i++;
+        qu[row].y=i+j++;
+    }
+
+    for ( i = 0; i < N; i++)
+    {
+        for ( j = 0; j < N; j++)
+        {
+printf("%d ",vMap[i][j]);
+        }
+        puts("");
+    }
+
+
+
+    for ( i = 0; i < 51; i++)
+    {
+        printf("x: %d y:%d",qu[i].x,qu[i].y);
+        puts("");
+    }
+
 
 }
